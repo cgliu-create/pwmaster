2ad4c2146a3c69e19265edad5ecad87ce023125e
cgliu-create <chrisliu2438@gmail.com>
2020-12-02 04:20:10 -0500

    clean

--- pwmaster/pwmanager/static/js/togglemodals.js
+++ pwmaster/pwmanager/static/js/togglemodals.js
@@ -9,6 +9,13 @@ function dontShowIt(id){
     element.style.display = 'none'
 }
 
+function closeNav(){
+    var x = document.getElementById("mobilenav");
+    if (x.style.display != "none") {
+      x.style.display = "none";
+    } 
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     if (document.getElementById('addpassword')){
         document.getElementById('addpassword').onclick = () => {
@@ -43,31 +50,37 @@ document.addEventListener("DOMContentLoaded", () => {
     if (document.getElementById('signinA')){
         document.getElementById('signinA').onclick = () => {
             showIt('signinform');
+            closeNav();
         };
     }
     if (document.getElementById('signinB')){
         document.getElementById('signinB').onclick = () => {
             showIt('signinform');
+            closeNav();
         };
     }
     if (document.getElementById('signupA')){
         document.getElementById('signupA').onclick = () => {
             showIt('signupform');
+            closeNav();
         };
     }
     if (document.getElementById('signupB')){
         document.getElementById('signupB').onclick = () => {
             showIt('signupform');
+            closeNav();
         };
     } 
     if (document.getElementById('signincancel')){
         document.getElementById('signincancel').onclick = () => {
             dontShowIt('signinform');
+            closeNav();
         };
     } 
     if (document.getElementById('signupcancel')){
         document.getElementById('signupcancel').onclick = () => {
             dontShowIt('signupform');
+            closeNav();
         };
     } 
 });
